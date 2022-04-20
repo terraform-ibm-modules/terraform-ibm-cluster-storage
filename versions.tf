4cd8a3b42fdfff9236a0da310b7eba46c92553ea
Kevin Silva <Kevin.Silva@ibm.com>
2022-04-19 20:27:04 -0400

    fix error for terraform.validate workflow - 3

--- versions.tf
+++ versions.tf
@@ -1,11 +1,12 @@
 #####################################################
-# activity tracker as a service
+# Cluster Storage as a service
 # Copyright 2020 IBM
 #####################################################
 
-/***************************************************
+/*
 NOTE: To source a particular version of IBM terraform
 provider version, configure the parameter `version`.
+*/
 
 terraform {
   required_version = ">=0.13"
@@ -19,15 +20,17 @@ terraform {
     }
   }
 }
+
+/*
 If we dont configure the version parameter,
 it fetches latest provider version.
-****************************************************/
-
 terraform {
   required_version = ">=0.13"
   required_providers {
     ibm = {
+
       source = "IBM-Cloud/ibm"
     }
   }
 }
+*/
