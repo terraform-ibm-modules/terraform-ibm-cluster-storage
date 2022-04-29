3c91df5a9356ac8e1143db0a9a687cffa2de53e0
Kevin Silva <Kevin.Silva@ibm.com>
2022-04-29 13:19:21 -0400

    fix error for terraform.validate workflow - 16

--- examples/odf/main.tf
+++ examples/odf/main.tf
@@ -1,5 +1,37 @@
-provider "ibm" {}
+provider "ibm" {
+  region = var.region
+}
 
+###################################################################
+# Read existing resource group
+###################################################################
+data "ibm_resource_group" "group" {
+  name = var.resource_group_name
+}
+
+###################################################################
+# Create kube config path
+###################################################################
+resource "null_resource" "mkdir_kubeconfig_dir" {
+  triggers = { always_run = timestamp() }
+  provisioner "local-exec" {
+    command = "mkdir -p ${var.kube_config_path}"
+  }
+}
+
+###################################################################
+# Read cluster config
+###################################################################
+data "ibm_container_cluster_config" "cluster_config" {
+  depends_on        = [null_resource.mkdir_kubeconfig_dir]
+  cluster_name_id   = var.cluster
+  resource_group_id = data.ibm_resource_group.group.id
+  config_dir        = var.kube_config_path
+}
+
+###################################################################
+# Create odf instance
+###################################################################
 // Module:
 
 module "odf" {
