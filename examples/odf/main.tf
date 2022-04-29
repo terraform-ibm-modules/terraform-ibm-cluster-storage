e230b1bf1b29940c6486291711c428d5cd774962
Kevin Silva <Kevin.Silva@ibm.com>
2022-04-29 13:06:47 -0400

    fix error for terraform.validate workflow - 13

--- examples/odf/main.tf
+++ examples/odf/main.tf
@@ -4,6 +4,12 @@ provider "ibm" {}
 
 module "odf" {
   source        = "./../.."
+
   is_enable_odf = var.is_enable_odf
   cluster       = var.cluster
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
+  resource_group   = var.resource_group_name
+  unique_id        = var.unique_id
+  kube_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
 }
