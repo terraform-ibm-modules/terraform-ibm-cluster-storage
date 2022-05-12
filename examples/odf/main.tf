2ede39bbd5fd5d1282b938b2b987b7a70518ad8d
Kevin Silva <Kevin.Silva@ibm.com>
2022-05-11 23:29:12 -0400

    code update - fix 8

--- examples/odf/main.tf
+++ examples/odf/main.tf
@@ -42,10 +42,11 @@ module "odf" {
   roks_version     = var.roks_version
   ibmcloud_api_key = var.ibmcloud_api_key
 
-  //region           = var.region
-  //resource_group   = var.resource_group_name
-  //unique_id        = var.unique_id
-  //kube_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
+  // Portworx parameters (added because git action complain)
+  region           = var.region
+  resource_group   = var.resource_group_name
+  unique_id        = var.unique_id
+  kube_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
 
   // ODF parameters
   monSize             = var.monSize
