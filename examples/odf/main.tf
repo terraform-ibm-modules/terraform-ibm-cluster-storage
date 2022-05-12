246b303c91ba05e382f907dae8e5d877d1081826
Kevin Silva <Kevin.Silva@ibm.com>
2022-05-11 23:24:01 -0400

    code update - fix 6

--- examples/odf/main.tf
+++ examples/odf/main.tf
@@ -40,10 +40,20 @@ module "odf" {
   is_enable_odf = var.is_enable_odf
   cluster       = var.cluster
   roks_version  = var.roks_version
-
   ibmcloud_api_key = var.ibmcloud_api_key
-  region           = var.region
-  resource_group   = var.resource_group_name
-  unique_id        = var.unique_id
-  kube_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
+
+  //region           = var.region
+  //resource_group   = var.resource_group_name
+  //unique_id        = var.unique_id
+  //kube_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
+
+  // ODF parameters
+  monSize = var.monSize
+  monStorageClassName = var.monStorageClassName
+  osdStorageClassName = var.osdStorageClassName
+  osdSize = var.osdSize
+  numOfOsd = var.numOfOsd
+  billingType = var.billingType
+  ocsUpgrade = var.ocsUpgrade
+  clusterEncryption = var.clusterEncryption
 }
