fb41de746fceddf2867f629ee7d46cbd7ba261f7
aman-chhabra1 <aman.chhabra@ibm.com>
2022-09-07 12:53:34 +0530

    Updating the secret baseline

--- modules/portworx_upgrade/main.tf
+++ modules/portworx_upgrade/main.tf
@@ -9,13 +9,12 @@ provider "helm" {
 }
 
 resource "helm_release" "px" {
-  repository        = "https://raw.githubusercontent.com/IBM/charts/master/repo/community"
-  chart             = "portworx"
-  name              = "portworx"
-  reuse_values      = true
-  dependency_update = true
-  force_update      = true
-  recreate_pods     = false
-  wait              = true
-  max_history       = 1
+  repository    = "https://raw.githubusercontent.com/IBM/charts/master/repo/community"
+  chart         = "portworx"
+  name          = "portworx"
+  reuse_values  = true
+  force_update  = true
+  recreate_pods = false
+  wait          = true
+  max_history   = 1
 }
